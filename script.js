b0cc4b1a24c0790002aa0720206be49d9534feeb
Anton <anton20020426@gmail.com>
2023-05-09 20:01:52 +0200

    Add player objects, track activePlayer, make moves

--- script.js
+++ script.js
@@ -1,49 +1,106 @@
-// modules for ONE of something
+// gameBoard creates a visual reprensentation of the gameboard in the console
 const gameBoard = (function () {
   const gameBoard = [];
   const rows = 3;
   const columns = 3;
+  let index = 1;
 
   for (let i = 0; i < rows; i++) {
-    gameBoard.push([]);
+    gameBoard[i] = [];
     for (let j = 0; j < columns; j++) {
-      gameBoard[i].push("X");
+      gameBoard[i].push(index);
+      index++;
     }
   }
 
-  return { gameBoard };
+  const getBoard = () => gameBoard;
+
+  return { getBoard };
 })();
 
-const displayController = (function () {})();
-
-// factory function for MULTIPLES of something
-const createPlayer = (name, token) => {
-  const fullName = () => {
-    console.log(`This player's name is ${name} with ${token}`);
+// what makes the game progress
+const displayController = (function () {
+  const createPlayer = (name, token) => {
+    return { name, token };
   };
 
-  return { fullName };
-};
+  const players = [
+    createPlayer("firstPlayer", "X"),
+    createPlayer("secondPlayer", "O"),
+  ];
 
-const player1 = createPlayer("Anton", "X");
-const player2 = createPlayer("Vadim", "O");
+  let activePlayer = players[0];
 
-// function to rended 'x's and 'o'x to the cells
-const renderGameboard = (function () {
-  const cells = document.querySelectorAll(".cell");
-  // let num = 0;
-  cells.forEach((cell) => {
-    // cell.textContent = gameBoard.gameBoard[num];
-    // num++;
+  const printMove = function () {
+    console.log(`It is ${activePlayer.name}'s turn`);
+    console.log(gameBoard.getBoard());
+  };
 
-    cell.addEventListener("click", leaveMark);
-  });
+  const switchPlayer = function () {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    console.log(`Player was switched - new player is ${activePlayer.name}`);
+  };
 
-  function leaveMark() {
-    if (this.textContent === "") {
-      this.textContent = "X";
-    } else {
-      console.log("the spot is taken");
+  const getActivePlayer = () => activePlayer;
+
+  const checkForWin = (row) => {
+    // win conditions
+    const rowWin = ["X", "X", "X"];
+
+    if (row.toString() === rowWin.toString()) {
+      console.log(`${getActivePlayer().name} won`);
+      return true;
     }
-  }
+  };
+
+  const usedCells = [];
+  const makeMove = function (cell) {
+    usedCells.forEach((cellNum) => {
+      if (cellNum === cell) {
+        console.log("This cell was taken already...");
+      }
+    });
+
+    gameBoard.getBoard().forEach((array) => {
+      for (let i = 0; i < 3; i++) {
+        if (array[i] === cell) {
+          array[i] = getActivePlayer().token;
+          usedCells.push(cell);
+          console.log(`${usedCells} was used`);
+
+          //if there are 3 x's in a row, then it is a win
+          if (checkForWin(array)) {
+            return;
+          } else {
+            switchPlayer();
+            printMove();
+          }
+        }
+      }
+    });
+  };
+
+  printMove();
+
+  return { makeMove, getActivePlayer };
 })();
+
+// // function to rended 'x's and 'o'x to the cells
+// const renderGameboard = (function () {
+//   const cells = document.querySelectorAll(".cell");
+//   // let num = 0;
+//   cells.forEach((cell) => {
+//     // cell.textContent = gameBoard.gameBoard[num];
+//     // num++;
+
+//     cell.addEventListener("click", leaveMark);
+//   });
+
+//   function leaveMark() {
+//     if (this.textContent === "") {
+//       this.textContent = "X";
+//     } else {
+//       console.log("the spot is taken");
+//     }
+//   }
+// })();
